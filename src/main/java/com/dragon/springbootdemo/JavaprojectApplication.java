dce1aad5394081505302eb21c096b2117090ee90
chenlonglyz <905037817@qq.com>
2020-07-30 14:42:57 +0800

    第一次提交

---
+++ src/main/java/com/dragon/springbootdemo/JavaprojectApplication.java
@@ -0,0 +1,13 @@
+package com.dragon.springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dragon/springbootdemo/JavaprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dragon.springbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
